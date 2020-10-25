1bee7537ab7c63cde3546d4fc98644298c5c43de
Sulistio Sanjaya <56523212+SulistioSanjaya20@users.noreply.github.com>
2020-10-25 11:02:17 +0700

    Add project files.

---
+++ Models/Peminjaman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_084.Models
+{
+    public partial class Peminjaman
+    {
+        public int IdPeminjaman { get; set; }
+        public DateTime? TglPeminjamaan { get; set; }
+        public int? IdKendaraan { get; set; }
+        public int? IdCostumer { get; set; }
+        public int? IdJaminan { get; set; }
+        public int? Biaya { get; set; }
+
+        public Kendaraan IdCostumerNavigation { get; set; }
+        public Pengembalian IdPeminjaman1 { get; set; }
+        public Customer IdPeminjamanNavigation { get; set; }
+        public Jaminan Jaminan { get; set; }
+    }
+}
