8a8bec0201d8b74c480722c58e85c2ac05b9a7bb
Sulistio Sanjaya <56523212+SulistioSanjaya20@users.noreply.github.com>
2020-10-31 11:59:58 +0700

    excersit 1

---
+++ Controllers/PengembaliansController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RentalKendaraan_084.Models;
+
+namespace RentalKendaraan_084.Controllers
+{
+    public class PengembaliansController : Controller
+    {
+        private readonly RentKendaraanContext _context;
+
+        public PengembaliansController(RentKendaraanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pengembalians
+        public async Task<IActionResult> Index()
+        {
+            var rentKendaraanContext = _context.Pengembalian.Include(p => p.IdPeminjamanNavigation);
+            return View(await rentKendaraanContext.ToListAsync());
+        }
+
+        // GET: Pengembalians/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pengembalian = await _context.Pengembalian
+                .Include(p => p.IdPeminjamanNavigation)
+                .FirstOrDefaultAsync(m => m.IdPengembalian == id);
+            if (pengembalian == null)
+            {
+                return NotFound();
+            }
+
+            return View(pengembalian);
+        }
+
+        // GET: Pengembalians/Create
+        public IActionResult Create()
+        {
+            ViewData["IdPeminjaman"] = new SelectList(_context.KondisiKendaraan, "IdKondisi", "IdKondisi");
+            return View();
+        }
+
+        // POST: Pengembalians/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPengembalian,TglPeminjaman,IdPeminjaman,IdKondisi,Denda")] Pengembalian pengembalian)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pengembalian);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdPeminjaman"] = new SelectList(_context.KondisiKendaraan, "IdKondisi", "IdKondisi", pengembalian.IdPeminjaman);
+            return View(pengembalian);
+        }
+
+        // GET: Pengembalians/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pengembalian = await _context.Pengembalian.FindAsync(id);
+            if (pengembalian == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdPeminjaman"] = new SelectList(_context.KondisiKendaraan, "IdKondisi", "IdKondisi", pengembalian.IdPeminjaman);
+            return View(pengembalian);
+        }
+
+        // POST: Pengembalians/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPengembalian,TglPeminjaman,IdPeminjaman,IdKondisi,Denda")] Pengembalian pengembalian)
+        {
+            if (id != pengembalian.IdPengembalian)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pengembalian);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PengembalianExists(pengembalian.IdPengembalian))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdPeminjaman"] = new SelectList(_context.KondisiKendaraan, "IdKondisi", "IdKondisi", pengembalian.IdPeminjaman);
+            return View(pengembalian);
+        }
+
+        // GET: Pengembalians/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pengembalian = await _context.Pengembalian
+                .Include(p => p.IdPeminjamanNavigation)
+                .FirstOrDefaultAsync(m => m.IdPengembalian == id);
+            if (pengembalian == null)
+            {
+                return NotFound();
+            }
+
+            return View(pengembalian);
+        }
+
+        // POST: Pengembalians/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pengembalian = await _context.Pengembalian.FindAsync(id);
+            _context.Pengembalian.Remove(pengembalian);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PengembalianExists(int id)
+        {
+            return _context.Pengembalian.Any(e => e.IdPengembalian == id);
+        }
+    }
+}
